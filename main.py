2ea69f1eee6360cb98006449a8657d1116f5b3d2
LEcombi <combi123@outlook.de>
2025-02-28 17:55:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as msgbox
 import pyperclip
 
 def copy_text():
@@ -12,21 +13,22 @@ def copy_text():
 
         output_label.config(text=entire_text)
         pyperclip.copy(entire_text)
+        msgbox.showinfo("Confirmation", "The text has been copied to the clipboard.")
     except ValueError:
-        output_label.config(text="Please enter a valid number for times.")
+        output_label.config(text="Please enter a valid number.")
 
 root = tk.Tk()
-root.title("Text Copier")
+root.title("Text Repeater")
 
 tk.Label(root, text="How many times would you like to write the text:").pack()
 times_entry = tk.Entry(root)
 times_entry.pack()
 
-tk.Label(root, text="What would you like to write:").pack()
+tk.Label(root, text="What text would you like to write:").pack()
 text_entry = tk.Entry(root)
 text_entry.pack()
 
-tk.Button(root, text="Copy", command=copy_text).pack()
+tk.Button(root, text="Repeat", command=copy_text).pack()
 
 output_label = tk.Label(root, text="", justify="left")
 output_label.pack()
